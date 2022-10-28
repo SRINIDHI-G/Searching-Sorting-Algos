f680a6db1b84fba0ca4216fa768b3f2b3fe28f30
G SRINIDHI <srinidhi.g@Infosys.com>
2022-10-28 19:01:17 +0530

    Added Binary Search code, Time Complexity

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,9 +1,41 @@
 package MergeSearch;
+public  void mySearch(int arr[], int Firstele, int Firstele, int key)
+{
+	int midele = (Firstele + Firstele)/2;
+	while( Firstele <= Firstele )
+   { 
+		if ( arr[midele] < key )
+	  {  
+        Firstele = midele + 1;     
+      }
+	  else if ( arr[midele] == key )
+	  { 
+		  Sysout("Print ele Found");
+		  break;
+	  }
+		
+		
+	else
+	{
+		Sysout("Print ele not found");
+	}
+		midele=(first+last)/2
+}
+}
 public class MergeSearch   
 {
  public static void main(String[] args) 
  {
 	/*Please start adding your code here*/
+	int arr[] = {10,20,30,40,50};  
+        int key = 30;  
+        int Firstele=arr.length-1;  
+        mySearch(arr,0,Firstele,key); 
+	 
 }
  
 }
+
+
+// Time COmplexity : O(logn)
+	
