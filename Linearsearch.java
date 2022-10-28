2b020c9d4537c0c7f795ae29b75345ed3191c43a
G SRINIDHI <42901700+SRINIDHI-G@users.noreply.github.com>
2022-10-28 19:13:41 +0530

    Updated Linear Search and time complexity

--- Linearsearch.java
+++ Linearsearch.java
@@ -1,9 +1,23 @@
 package LinearSearch;
 public class linearSearch 
 {
+  public voide lsearch(int[]ar,int key)
+  {
+	  for(int i=0;i<key.length;i++)
+	  {
+		  if(ar[i]==key)
+			  return i;
+	  }
+	  return -1
+  }
  public static void main(String[] args) 
  {
 	/*Please start adding your code here*/
+	 int[] a={10,20,30,40,40,60};
+	 int key=50;
+	 Sysout("lsearch(a,key)");
+	 
+	 
 }
  
 }
